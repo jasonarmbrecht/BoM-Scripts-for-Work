3fb2fffb10f4b1359961b525cb8b36fafe986be3
jasonarmbrecht <jason.armbrecht@gmail.com>
2022-04-02 23:47:38 +1100

    Update BoM_VIC_Package.py
    Major developmental changes.

--- BoM_VIC_Package.py
+++ BoM_VIC_Package.py
@@ -1,10 +1,12 @@
 # BoM Downloader for Kordia MSCS
-# Version 0.1
+# Version 0.2
 # By Jason Armbrecht
 
-import wget, os, shutil, time, psutil, sys
+import wget, os, shutil, time, psutil, sys, fitz
 from datetime import datetime
 from txtmarker.factory import Factory
+from colorama import init
+init()
 
 # Defined variables. Change if you need.
 bomDir = r'C:\BoMTemp\\'
@@ -16,113 +18,139 @@ now = datetime.now()
 
 # Check if BoMTemp exists. If not, create folder
 def checkFolder ():
-    print(now, "Checking for BoMTemp folder...")
+    print(now, "\x1b[1;33;40m Checking for BoMTemp folder...\x1b[1;37;40m")
     CHECK_FOLDER = os.path.isdir(bomDir)
     if not CHECK_FOLDER: # If folder doesn't exist, then create it.
         os.makedirs(bomDir)
-        print(now, "created folder : ", bomDir)
+        print(now, "\x1b[1;32;40m created folder : \x1b[1;37;40m", bomDir)
     else:
-        print(now, bomDir, "folder already exists.")
+        print(now, "\x1b[1;32;40m", "", bomDir, "folder already exists.\x1b[1;37;40m")
 
 # Delete all files in BoMTemp
 def deleteFiles ():
-    print(now, "Cleaning BoMTemp folder.")
+    print(now, "\x1b[1;33;40m Cleaning BoMTemp folder...\x1b[1;37;40m")
     for filename in os.listdir(bomDir):
         file_path = os.path.join(bomDir, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-                print(now, "Cleaning directory...")
+                print(now, "\x1b[1;32;40m File removed.\x1b[1;37;40m")
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-                print(now, "Directory cleaned.")
+                print(now, "\x1b[1;32;40m Directory cleaned.\x1b[1;37;40m")
         except Exception as e:
             print(now, 'Failed to delete %s. Reason: %s' % (file_path, e))
 
 # Download .txt file to retrive update time
 def downloadTxt (bomId):
-    print(now, "Requesting forcast issue time...")
+    print("\n", now, "\x1b[1;33;40m Requesting forecast issue time...\x1b[1;37;40m", "\n")
     wget.download('ftp://ftp2.bom.gov.au/anon/gen/fwo/' + bomId + '.txt', bomDir)
 
 # Print the time updated line
 def printUpdate (bomId):
+    print("\x1b[1;34;40m")
     file = open(bomDir + bomId + ".txt")
     content = file.readlines() # read the content of the file opened
-    print(" ")
-    print(content[4], content[5]) # read 10th line from the file
+    #print(" ")
+    print(content[4], content[5]) # read the 4th and 5th line from the file
 
 # Asks user to confirm if the FTP has actually been updated yet.
 def confirmUpdate ():
-    answer = input("Check issue time above. Continue with the download? (y/n): ") 
+    print("\x1b[1;31;40m Confirm the issue times above. \x1b[1;37;40m")
+    answer = input("Continue with the download? (y/n): ") 
     if answer == "y": 
-        print("Beginning download and printing of files:") 
+        print("\x1b[1;33;40m Beginning download, highlighting and printing of files:\x1b[1;37;40m") 
     elif answer == "n": 
         sys.exit() 
     else: 
-        print("Please enter y or n.")
+        print("\x1b[1;37;40m Please enter y or n.")
 
 # Download pdf files
 def downloadPdf (bomId):
-    print(now, "Starting download of ", bomId, "...")
+    print(now, "\x1b[1;33;40m Starting download of", bomId, "...\x1b[1;37;40m", "\n")
     wget.download('ftp://ftp2.bom.gov.au/anon/gen/fwo/' + bomId + '.pdf', bomDir)
-    print(now, bomId, "pdf file finished downloading.")
+    print("\n", "\x1b[1;32;40m", now, bomId, "pdf file finished downloading.\x1b[1;37;40m", "\n")
 
-# Create highlighted annotations
 def highlightDoc (bomId):
-    highlights = [
-        ("Wind Warning", "Strong Wind Warning"),
-        ("Wind Warning", "Gale Warning"),
-        (None, "Issued at"),
-        (None, "Forecast issued at"),
-        (None, "Please be aware"),
-        (None, "Weather Situation"),
-        (None, "West Coast: SA-VIC Border to Cape Otway"),
-        (None, "Central Coast: Cape Otway to Wilsons Promontory"),
-        (None, "Central Gippsland Coast: Wilsons Promontory to Lakes Entrance"),
-        (None, "East Gippsland Coast: Lakes Entrance to 60nm east of Gabo Island"),
-    ]
-    highlighter = Factory.create("pdf")
-    highlighter.highlight(bomDir + bomId + ".pdf", bomDir + bomId + "_anno.pdf", highlights)
+    print("\x1b[1;33;40m Auto-highlighting", bomId, "...\n")
+    redColour = [0.8, 0.1, 0.1]
+    blueColour = [0.1, 0.9, 1]
+    greenColour = [0.6, 1, 0.1]
+    purpleColour = [0.6, 0.3, 1]
+    orangeColour = [1, 0.4, 0.1]
+    def hl (hlText, hlColour):
+        for page in pdf_file:
+            text_to_be_highlighted = hlText
+            highlight = page.search_for(text_to_be_highlighted)
+            for inst in highlight:
+                highlight = page.add_highlight_annot(inst)
+                highlight.set_colors(stroke = hlColour)
+                highlight.update()
+    pdf_file = fitz.open(bomDir + bomId + ".pdf")
+    hl("Strong Wind Warning", orangeColour)
+    hl("Gale Warning", redColour)
+    hl("Storm Force Wind Warning", redColour)
+    hl("Please be aware", blueColour)
+    hl("West Coast: SA-VIC Border to Cape Otway", greenColour)
+    hl("Central Coast: Cape Otway to Wilsons Promontory", greenColour)
+    hl("Central Gippsland Coast: Wilsons Promontory to Lakes Entrance", greenColour)
+    hl("East Gippsland Coast: Lakes Entrance to 60nm east of Gabo Island", greenColour)
+    hl("Coastal Waters Forecast for Victoria", purpleColour)
+    hl("Local Waters Forecast for Port Phillip", purpleColour)
+    hl("Local Waters Forecast for Western Port", purpleColour)
+    hl("Local Waters Forecast Gippsland Lakes", purpleColour)
+    pdf_file.save(bomDir + bomId + "_hl.pdf", garbage=4, deflate=True, clean=True)
+    print("\x1b[1;32;40m Document succesfully highlighted. \n")
 
 # Auto print files, kill adobe afterwards
 def printFiles (bomId):
-    print(now, "Temporarily opening PDF Reader...")
-    os.startfile(bomDir + bomId + '_anno.pdf', "print")
-    time.sleep(12)
+    print(now, "\x1b[1;33;40m Temporarily opening PDF Reader...\x1b[1;37;40m")
+    os.startfile(bomDir + bomId + '_hl.pdf', "print")
+    time.sleep(8)
     for p in psutil.process_iter(): # Close Acrobat after printing the PDF
         if 'AcroRd' in str(p):
             p.kill()
-            print(now, "Job sent to printer...")
+            print(now, "\x1b[1;32;40m Job sent to printer...\x1b[1;37;40m")
 
 
 # Main program start
-print("-------------------------------")
-print("***BoM Downloader for Kordia***")
-print("-------------------------------")
-print(" ")
-print("By Jason Armbrecht")
-print(" ")
+print("\x1b[1;34;40m    ____  ____  __  ___   ____                      __                __")         
+print("   / __ )/ __ \/  |/  /  / __ \____ _      ______  / /___  ____ _____/ /__  _____")
+print("  / __  / / / / /|_/ /  / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / _ \/ ___/")
+print(" / /_/ / /_/ / /  / /  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /  ")  
+print("/_____/\____/_/  /_/  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/    ") 
+print("\n")                                                                               
+print("\x1b[1;37;40m -------------------------------\x1b[1;37;40m")
+print("\x1b[1;34;40m ***BoM Downloader for Kordia***\x1b[1;37;40m")
+print("\x1b[1;37;40m -------------------------------\x1b[1;37;40m")
+print("\x1b[1;35;40m By Jason Armbrecht\x1b[1;37;40m")
+print("\n")
 checkFolder()
 deleteFiles()
 downloadTxt(coastal_vic)
 downloadTxt(local_portp)
 downloadTxt(local_wport)
 downloadTxt(local_gipps)
+print("\n")
 printUpdate(coastal_vic)
 printUpdate(local_portp)
 printUpdate(local_wport)
 printUpdate(local_gipps)
 confirmUpdate()
+print("\n")
 downloadPdf(coastal_vic)
 downloadPdf(local_portp)
 downloadPdf(local_wport)
 downloadPdf(local_gipps)
+print("\n")
 highlightDoc(coastal_vic)
 highlightDoc(local_portp)
 highlightDoc(local_wport)
 highlightDoc(local_gipps)
-printFiles(coastal_vic)
-printFiles(local_portp)
-printFiles(local_wport)
-printFiles(local_gipps)
-print("All tasks completed.")
+#printFiles(coastal_vic)
+#printFiles(local_portp)
+#printFiles(local_wport)
+#printFiles(local_gipps)
+sleep(3)
+deleteFiles()
+print("\n", "\x1b[1;32;40m All tasks completed.\x1b[1;37;40m")
