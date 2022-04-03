f085d8f513405a551127da3af70c1f9778e7a171
jasonarmbrecht <jason.armbrecht@gmail.com>
2022-04-04 07:40:37 +1000

    Update BoM_VIC_Package.py
    Changed to f strings.
    Changed text colouring behavior to make it easier to read.
    Tidied up small issues.

--- BoM_VIC_Package.py
+++ BoM_VIC_Package.py
@@ -1,10 +1,17 @@
+"""BOM DOWNLOADER"""
 # BoM Downloader for Kordia
-# Version 0.2
+# Version 0.3
 # By Jason Armbrecht
 # https://github.com/jasonarmbrecht/BoM-Scripts-for-Work
 
-import wget, os, shutil, time, psutil, sys, fitz
+import os
+import sys
+import shutil
+import time
 from datetime import datetime
+import psutil
+import fitz
+import wget
 from colorama import init
 init()
 
@@ -15,70 +22,77 @@ local_portp = 'IDV10460' # Port Phillip Local Waters
 local_wport = 'IDV10461' # Westerport Local Waters
 local_gipps = 'IDV19300' # Gippsland Lakes Local Waters
 
+#Define text colours.
+txt_grn = "\x1b[1;32;40m"
+txt_wht = "\x1b[1;37;40m"
+txt_ylo = "\x1b[1;33;40m"
+txt_blu = "\x1b[1;34;40m"
+txt_red = "\x1b[1;31;40m"
+txt_pur = "\x1b[1;35;40m"
 
 # Check if BoMTemp exists. If not, create folder
 def checkFolder ():
-    print(datetime.now(), "\x1b[1;33;40m Checking for BoMTemp folder...\x1b[1;37;40m")
-    CHECK_FOLDER = os.path.isdir(bomDir)
-    if not CHECK_FOLDER: # If folder doesn't exist, then create it.
+    print(f"{datetime.now()} {txt_ylo} Checking for BoMTemp folder...{txt_wht}")
+    check_folder = os.path.isdir(bomDir)
+    if not check_folder: # If folder doesn't exist, then create it.
         os.makedirs(bomDir)
-        print(datetime.now(), "\x1b[1;32;40m created folder : \x1b[1;37;40m", bomDir)
+        print(f"{datetime.now()} {txt_grn} created folder : {txt_wht} {bomDir}")
     else:
-        print(datetime.now(), "\x1b[1;32;40m", "", bomDir, "folder already exists.\x1b[1;37;40m")
+        print(f"{datetime.now()} {txt_grn} {bomDir} folder already exists.{txt_wht}")
 
 # Delete all files in BoMTemp
 def deleteFiles ():
-    print(datetime.now(), "\x1b[1;33;40m Cleaning BoMTemp folder...\x1b[1;37;40m")
+    print(f"{datetime.now()} {txt_ylo} Cleaning BoMTemp folder...{txt_wht}")
     for filename in os.listdir(bomDir):
         file_path = os.path.join(bomDir, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-                print(datetime.now(), "\x1b[1;32;40m File removed.\x1b[1;37;40m")
+                print(f"{datetime.now()} {txt_grn} File removed.{txt_wht}")
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-                print(datetime.now(), "\x1b[1;32;40m Directory cleaned.\x1b[1;37;40m")
+                print(f"{datetime.now()} {txt_grn} Directory cleaned.{txt_wht}")
         except Exception as e:
-            print(datetime.now(), 'Failed to delete %s. Reason: %s' % (file_path, e))
+            print(f"{datetime.now()} Failed to delete {file_path}. Reason: {e}")
 
 # Download .txt file to retrive update time
 def downloadTxt (bomId):
-    print(datetime.now(), "\x1b[1;33;40m Requesting forecast issue time...\x1b[1;37;40m")
-    wget.download('ftp://ftp2.bom.gov.au/anon/gen/fwo/' + bomId + '.txt', bomDir) # download from FTP server
+    print(f"{datetime.now()} {txt_ylo} Requesting forecast issue time...{txt_wht}")
+    wget.download('ftp://ftp2.bom.gov.au/anon/gen/fwo/' + bomId + '.txt', bomDir)
     print("\n")
 
 # Print the time updated line
 def printUpdate (bomId):
-    print("\x1b[1;34;40m")
-    file = open(bomDir + bomId + ".txt")
+    print(f"{txt_blu}")
+    file = open(bomDir + bomId + ".txt", encoding="utf8")
     content = file.readlines() # read the content of the file opened
     print(content[4], content[5]) # read the 4th and 5th line from the file
 
 # get the issue time to use for highlighting
 def issueTime (bomId):
-    file = open(bomDir + bomId + ".txt")
-    content = file.readlines() 
+    file = open(bomDir + bomId + ".txt", encoding="utf8")
+    content = file.readlines()
     return str(content[5]) # returns issue time sentence as string
 
 # Asks user to confirm if the FTP has actually been updated yet.
 def confirmUpdate ():
-    print("\x1b[1;31;40m Confirm the issue times above. \x1b[1;37;40m")
-    answer = input("Continue with the download? (y/n): ") 
-    if answer == "y": 
-        print("\n", "\x1b[1;33;40m Beginning download, highlighting and printing of files:\x1b[1;37;40m") 
-    elif answer == "n": 
-        sys.exit() 
-    else: 
-        print("\x1b[1;37;40m Please enter y or n.")
+    print(f" Confirm the issue times above. {txt_wht}")
+    answer = input("Continue with the download? (y/n): ")
+    if answer == "y":
+        print(f"\n {txt_ylo} Beginning download, highlighting and printing of files:{txt_wht}")
+    elif answer == "n":
+        sys.exit()
+    else:
+        print(f"{txt_wht} Please enter y or n.")
 
 # Download pdf files
 def downloadPdf (bomId):
-    print(datetime.now(), "\x1b[1;33;40m Starting download of", bomId, "...\x1b[1;37;40m")
+    print(f"{datetime.now()} {txt_ylo} Starting download of {bomId}...{txt_wht}")
     wget.download('ftp://ftp2.bom.gov.au/anon/gen/fwo/' + bomId + '.pdf', bomDir)
-    print("\n", datetime.now(), "\x1b[1;32;40m", bomId, "pdf file finished downloading.\x1b[1;37;40m",)
+    print(f"\n {datetime.now()} {txt_grn} {bomId} pdf file finished downloading.{txt_wht}")
 
 def highlightDoc (bomId):
-    print(datetime.now(), "\x1b[1;33;40m Auto-highlighting", bomId, "...")
+    print(f"{datetime.now()} {txt_ylo} Auto-highlighting {bomId}...")
     redColour = [1, 0.2, 0.2] # set colors to use for highlighting
     blueColour = [0.6, 0.6, 1]
     greenColour = [0.6, 1, 0.1]
@@ -110,30 +124,30 @@ def highlightDoc (bomId):
     hl("Local Waters Forecast for Western Port", yellowColour)
     hl("Local Waters Forecast Gippsland Lakes", yellowColour)
     pdf_file.save(bomDir + bomId + "_hl.pdf", garbage=4, deflate=True, clean=True)
-    print("\x1b[1;32;40m Document succesfully highlighted.", "\x1b[1;37;40m")
+    print(f"{txt_grn} Document succesfully highlighted.{txt_wht}")
 
 # Auto print files, kill adobe afterwards
 def printFiles (bomId):
-    print(datetime.now(), "\x1b[1;33;40m Temporarily opening PDF Reader...\x1b[1;37;40m")
+    print(f"{datetime.now()} {txt_ylo} Temporarily opening PDF Reader...{txt_wht}")
     os.startfile(bomDir + bomId + '_hl.pdf', "print")
     time.sleep(8)
     for p in psutil.process_iter(): # Close Acrobat after printing the PDF
         if 'AcroRd' in str(p):
             p.kill()
-            print(datetime.now(), "\x1b[1;32;40m Job sent to printer...\x1b[1;37;40m")
+            print(f"{datetime.now()} {txt_grn} Job sent to printer...{txt_wht}")
 
 
 # Main program start
-print("\x1b[1;34;40m    ____  ____  __  ___   ____                      __                __")         
+print(f"{txt_blu}    ____  ____  __  ___   ____                      __                __")
 print("   / __ )/ __ \/  |/  /  / __ \____ _      ______  / /___  ____ _____/ /__  _____")
 print("  / __  / / / / /|_/ /  / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / _ \/ ___/")
-print(" / /_/ / /_/ / /  / /  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /  ")  
-print("/_____/\____/_/  /_/  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/    ") 
-print("\n")                                                                               
-print("\x1b[1;37;40m -------------------------------\x1b[1;37;40m")
-print("\x1b[1;34;40m ***BoM Downloader for Kordia***\x1b[1;37;40m")
-print("\x1b[1;37;40m -------------------------------\x1b[1;37;40m")
-print("\x1b[1;35;40m By Jason Armbrecht\x1b[1;37;40m")
+print(" / /_/ / /_/ / /  / /  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /  ")
+print("/_____/\____/_/  /_/  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/    ")
+print("\n")
+print(f"{txt_wht} -------------------------------{txt_wht}")
+print(f"{txt_blu} ***BoM Downloader for Kordia***{txt_wht}")
+print(f"{txt_wht} -------------------------------{txt_wht}")
+print(f"{txt_pur} By Jason Armbrecht{txt_wht}")
 print("\n")
 checkFolder()
 deleteFiles()
@@ -163,4 +177,4 @@ printFiles(local_wport)
 printFiles(local_gipps)
 time.sleep(2)
 deleteFiles()
-print("\n", "\x1b[1;32;40m All tasks completed.\x1b[1;37;40m")
+print(f"\n {txt_grn} All tasks completed.{txt_wht}")
